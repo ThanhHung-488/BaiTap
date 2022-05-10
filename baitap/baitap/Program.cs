2c532255653733cb872ff356f9c2a3ab7ef1eddc
ThanhHung-488 <thanhhung14072001@gmail.com>
2022-05-10 07:41:51 +0700

    nopbai

---
+++ baitap/baitap/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace BaiTap
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] arr226 = new int[10];
+            int i226, mx226, mn226, n226;
+
+            Console.WriteLine("Nhap {0} phan tu vao trong mang:\n",n226);
+            n226 = Convert.ToInt32(Console.ReadLine());
+
+            for (i226 = 0; i226 < n226; i226++)
+            {
+                Console.Write("Phan tu - {0}: ", i226);
+                arr226[i226] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            mx226 = arr226[0];
+            mn226 = arr226[0];
+
+            for (i226 = 1; i226 < n226; i226++)
+            {
+                if (arr226[i226] > mx226)
+                {
+                    mx226 = arr226[i226];
+                }
+
+
+                if (arr226[i226] < mn226)
+                {
+                    mn226 = arr226[i226];
+                }
+            }
+            Console.Write("Phan tu lon nhat trong mang la: {0}\n", mx226);
+            Console.Write("Phan tu nho nhat trong mang la: {0}\n\n", mn226);
+
+            Console.ReadKey();
+        }
+    }
+}
